ff33924f3cd4354f4cf233c525b19b774793c674
Searace15 <120301872+Searace15@users.noreply.github.com>
2022-12-11 13:21:09 +0500

    Add files via upload

---
+++ Script.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Вывод приветсвия
+        print("Hello, World!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
